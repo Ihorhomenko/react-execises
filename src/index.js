25f8404ed3b954799dd824724a6cc8a400f4ab4c
Ihor Khomenko <igorhomenko97@gmail.com>
2022-09-27 12:04:21 +0300

    start new project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/react-execises/">
     <App />
   </React.StrictMode>
 );
